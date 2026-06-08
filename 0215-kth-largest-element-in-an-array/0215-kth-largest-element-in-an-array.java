06b2d3e251d3d2716adae9080c6c07fb638460d3
Hafeez Mohammad <hafeezmohammad689@gmail.com>
2026-06-08 12:14:41 +0530

    Time: 2182 ms (5.19%), Space: 80.4 MB (5.09%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,16 +1,46 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        for (int num : nums) {
+    public int findKthLargest(int[] nums, int k) {
+
+        int targetIndex = nums.length - k ;
+        int low =0;
+        int high = nums.length-1;
+        return quickSelect(nums,low,high,targetIndex);
         
-            minHeap.offer(num);
-    
-            if (minHeap.size() > k) {
-                minHeap.poll();
+    }
+
+    public int quickSelect ( int[] nums , int low , int high , int targetIndex){
+        if(low == high){
+            return nums[low];
+        }
+       
+        int sortedIndex = partition(nums,low,high);
+        if(targetIndex==sortedIndex){
+            return nums[sortedIndex];
+        }else if (targetIndex<sortedIndex){
+            return quickSelect(nums,low,sortedIndex-1,targetIndex);
+        }else{
+            return quickSelect(nums,sortedIndex+1 , high , targetIndex);
+        }
+
+    }
+
+    public int partition(int[] nums , int low , int high){
+         int i = low-1;
+        int pivot = nums[high];
+        for(int j =low; j<high ; j++){
+            if(nums[j] < pivot){
+                i++;
+                swap(nums,i,j);
             }
         }
-      
-        return minHeap.poll();
+        swap(nums,i+1,high);
+        return i+1;
+    }
+
+    public void swap(int[] nums , int i , int j){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
