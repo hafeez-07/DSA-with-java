6b3bb1aa9d03a86a057c8db6631b167b9fbe20dd
Hafeez Mohammad <hafeezmohammad689@gmail.com>
2026-07-03 05:35:05 +0530

    Time: 2 ms (16.3%), Space: 43.3 MB (14.71%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         if(s.length() != goal.length()) return false;
         String str = s+s;
-        if(str.contains(goal)) return true;
-        return false;
+        return str.contains(goal);
     }
 }
