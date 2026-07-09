4bb53264b7502294787bcb856445197fedd5ae27
Hafeez Mohammad <hafeezmohammad689@gmail.com>
2026-07-09 10:07:04 +0530

    Time: 0 ms (100%), Space: 42.7 MB (84.45%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,22 +1,25 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        if(s1.length() != s2.length()) return false;
-        int[] freq = new int[26];
-        int diff =0;
-        for(int i=0; i<s1.length(); i++){
-            freq[s1.charAt(i)-'a']++;
-            freq[s2.charAt(i)-'a']--;
-            if(s1.charAt(i) != s2.charAt(i)) diff++;
-        }
-        //1. if freq of letters in both string is not same , then we cannot swap and find answer
-        for(int x: freq){
-            if(x!=0)  return false;  
-        }
+      int first = -1 ; //first mismatched index
+      int second = -1; // second mismatched index
 
-        //2.if char diff is 0 or 2 , then its true , we can swap 2 letters and make string equal
-       if(diff == 0 || diff == 2) return true;
+      for(int i=0; i<s1.length(); i++){
+        if(s1.charAt(i) != s2.charAt(i)){
+            if(first == -1){
+                first=i;
+            }else if(second == -1){
+                second=i;
+            }else{
+                return false;  //more than 2 mismatch
+            }
+        }
+      }
 
-       return false;
+      if(first == -1) return true; //no mismatch
+
+      if(second == -1) return false; //only 1 mismatch
+
+      return s1.charAt(first) == s2.charAt(second) && s1.charAt(second) == s2.charAt(first);
 
     }
 }
