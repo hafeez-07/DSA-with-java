e5d3d8b4fae18e980f9a4cb2f4f89772876fd039
Hafeez Mohammad <hafeezmohammad689@gmail.com>
2026-07-26 08:40:15 +0530

    Time: 2 ms (99.95%), Space: 52.5 MB (66.26%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,19 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
         List<Integer> result = new ArrayList<>();
-
-        //count frequency
+        int candidate1 = 0;
+        int count1 = 0;
+        int candidate2 = 0;
+        int count2 = 0;
         for(int x : nums){
-            map.put(x,map.getOrDefault(x,0)+1);
+            //check is this one of candidate
+          if(x == candidate1) count1++;
+          else if (x == candidate2) count2++;
+          //else check do i have an empty slot for canidates
+          else if (count1 == 0){
+            candidate1 = x;
+            count1 = 1;
+          }else if( count2 == 0){
+            candidate2 = x;
+            count2 = 1;
+            //else compete for seats
+          }else{
+            count1--;
+            count2--;
+          }
         }
 
-        int n = nums.length;
-
-        //check whether any element appear more than n/3 times
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            if(entry.getValue() > n / 3) result.add(entry.getKey());
+        //check whether 2 canidates are actually appear more than n/3 times
+        count1 = 0;
+        count2 = 0;
+        for(int x : nums){
+            if(x == candidate1) count1++;
+            else if(x == candidate2) count2++;
         }
+        if(count1 > nums.length / 3) result.add(candidate1);
+        if(count2 > nums.length / 3) result.add(candidate2);
 
         return result;
 
