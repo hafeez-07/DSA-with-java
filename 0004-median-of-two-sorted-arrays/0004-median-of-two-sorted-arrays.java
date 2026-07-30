1037ac8a780e79c06c7a60010b70850ef0d84125
Hafeez Mohammad <hafeezmohammad689@gmail.com>
2026-07-30 17:29:18 +0530

    Time: 1 ms (100%), Space: 48.9 MB (52.81%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,35 +1,37 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-         int m = nums1.length;
+        int m = nums1.length;
         int n = nums2.length;
-        int i = 0, j = 0, k = 0;
-        int[] mergedArray = new int[m + n];
 
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                mergedArray[k++] = nums1[i++];
-            } else {
-                mergedArray[k++] = nums2[j++];
+        //swap the array if nums1 is larger - to ensure that binary partition is smaller and cut2 is in range
+        if( m > n) {
+            return findMedianSortedArrays(nums2,nums1);
+        }
+
+        int leftSize = (m + n + 1) / 2;
+        int left = 0;
+        int right = m;
+
+        while (left <= right){
+            int cut1 = ( left + right ) / 2;
+            int cut2 = leftSize - cut1;
+            int L1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int R1 = cut1 == m ? Integer.MAX_VALUE : nums1[cut1];
+            int L2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2-1];
+            int R2 = cut2 == n ? Integer.MAX_VALUE : nums2[cut2];
+
+            if(L1 <= R2 && L2 <= R1){ //correct partition
+                if((m + n) % 2 == 0){
+                    return (Math.max(L1,L2) + Math.min(R1,R2)) / 2.0 ;
+                }else{
+                    return Math.max(L1,L2);
+                }
+            }else if (L1 > R2){
+                right =  cut1 - 1;
+            }else{
+                left = cut1 + 1;
             }
         }
-        while (i < m) {
-            mergedArray[k++] = nums1[i++];
-        }
-        while (j < n) {
-            mergedArray[k++] = nums2[j++];
-        }
-        for (int x : mergedArray) {
-            System.out.print(x + " ");
-        }
-
-       
-        int total = m+n;
-        if (total%2 ==0) {
-            int mid1 = total/2 - 1;
-            int mid2 = total/2;
-            return ((mergedArray[mid1] + mergedArray[mid2]) / 2.0);
-        }
-        return mergedArray[total / 2];
-        
+        return 0.0;
     }
 }
