7251bfce2c5b59ee79a9e0e6b6812c7c6a955d99
Hafeez Mohammad <hafeezmohammad689@gmail.com>
2026-08-17 11:26:11 +0530

    Time: 2 ms (93.25%), Space: 46.5 MB (85.86%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -10,13 +10,20 @@ class Solution {
 
         for(int i=0 ; i < mat.length ; i++){
             int count = 0;
-            for(int j=0 ; j < mat[0].length ; j++){
-                if(mat[i][j] == 1){
-                    count++;
+            int low = 0;
+            int high = mat[0].length;
+
+            //search for first zero
+            while(low < high){
+                int mid = low + (high - low)/2;
+                if(mat[i][mid]==1){
+                    low = mid + 1;
                 }else{
-                    break;
+                    high = mid;
                 }
             }
+            count = low;
+
             maxHeap.offer(new int[]{count,i}); //if all rows are 1
             if(maxHeap.size() > k){
                 maxHeap.poll();
