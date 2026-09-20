0144fcbd64d0514c3d5eb373a8820f2c36194f48
Hafeez Mohammad <hafeezmohammad689@gmail.com>
2026-09-20 18:38:28 +0530

    Time: 2 ms (73.05%), Space: 42.8 MB (63.68%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -7,22 +7,24 @@ class Solution {
         int tRight = t.length()-1;
 
         while(sLeft <= sRight && tLeft <= tRight){
+            //move both left pointers when u find a match
             if(s.charAt(sLeft) == t.charAt(tLeft)){
                 sLeft++;
                 tLeft++;
                 counter--;
                 if(counter==0) return true;
+            //move both right pointer when u find a match
             }else if(s.charAt(sRight) == t.charAt(tRight)){
                 sRight--;
                 tRight--;
                 counter--;
                 if(counter==0) return true;
-            }else if(s.charAt(sLeft) != t.charAt(tLeft)){
+                //move only t pointer to search matching char in order
+            }else{
                 tLeft++;
-            }else if(s.charAt(sRight) != t.charAt(tRight)){
-                tRight--;
             }
         }
+        //check whether u have found the subsequence
         return counter==0;
     }
 }
